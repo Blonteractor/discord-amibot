fa736e43efa21f44fbd163d0810ba64d2df04985
Blonteractor <priyanv2003@gmail.com>
2023-04-28 00:15:42 +0530

    setup: Add boilerplate for basic discord bot

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use poise::serenity_prelude as serenity;
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            prefix_options: poise::PrefixFrameworkOptions {
+                prefix: Some("~".into()),
+                case_insensitive_commands: true,
+                ..Default::default()
+            },
+            commands: vec![age(), register()],
+            ..Default::default()
+        })
+        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::all())
+        .setup(|_, _ready, _| {
+            Box::pin(async move {
+                println!("Amibot is ready");
+                Ok(Data {})
+            })
+        });
+
+    framework.run().await.unwrap();
 }
