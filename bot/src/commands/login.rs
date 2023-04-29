3403818e8faac5467ac6b0225dcc6b1122b3fb95
Blonteractor <priyanv2003@gmail.com>
2023-04-29 18:04:08 +0530

    docs: Add docs for login command

--- bot/src/commands/login.rs
+++ bot/src/commands/login.rs
@@ -2,8 +2,12 @@ use amizone::api::user::User;
 
 use crate::{Context, Error};
 
-#[poise::command(slash_command, prefix_command)]
-pub async fn login(ctx: Context<'_>, username: String, password: String) -> Result<(), Error> {
+#[poise::command(slash_command, help_text_fn = "login_help", check = "login_check")]
+pub async fn login(
+    ctx: Context<'_>,
+    #[description = "Your amizone username"] username: String,
+    #[description = "Your amizone password"] password: String,
+) -> Result<(), Error> {
     let db_client = &ctx.data().connections.db;
     let amizone_conn = &ctx.data().connections.amizone;
     let caller_id = ctx.author().id.to_string();
@@ -15,11 +19,41 @@ pub async fn login(ctx: Context<'_>, username: String, password: String) -> Resu
     // Random call to the api to see if the credentials are correct
     if amizone_client.get_user_profile().await.is_err() {
         ctx.say("Incorrect credentials.").await?;
-        User::forget(&caller_id, db_client).await?;
+        User::forget(caller_id, db_client).await?;
     } else {
         ctx.say("Login successfull, use the help command to get started.")
             .await?;
     }
 
     Ok(())
+}
+
+fn login_help() -> String {
+    "/login - Log into Amizone with your credentials.
+
+Usage: /login [username] [password]
+
+Arguments:
+- [username]: Your Amizone username.
+- [password]: Your Amizone password.
+
+Example:
+/login johnsmith password123
+
+Note: Your credentials are securely stored using encryption."
+        .into()
+}
+
+async fn login_check(ctx: Context<'_>) -> Result<bool, Error> {
+    if User::from_id(ctx.author().id.to_string(), &ctx.data().connections.db)
+        .await?
+        .is_some()
+    {
+        ctx.say("You are already logged in and ready to go.")
+            .await?;
+
+        Ok(false)
+    } else {
+        Ok(true)
+    }
 }
