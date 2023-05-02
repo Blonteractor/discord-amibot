ad1deea5b4be659101decd7542f601cf8082f1f0
Blonteractor <priyanv2003@gmail.com>
2023-05-02 16:20:32 +0530

    feat: Credentials are now encrypted using AES256

--- amizone/src/api/user/credentials.rs
+++ amizone/src/api/user/credentials.rs
@@ -1,9 +1,36 @@
-use base64::{engine::general_purpose::URL_SAFE, DecodeError, Engine as _};
+use base64::{
+    engine::general_purpose::{STANDARD, URL_SAFE},
+    DecodeError, Engine as _,
+};
 use mongodb::bson::{doc, Bson};
+use once_cell::sync::Lazy;
 use serde::ser::{Serialize, SerializeStruct};
 use tonic::metadata::AsciiMetadataValue;
 
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Nonce,
+};
+
+use rand::RngCore;
+
 pub type UserMetaData = AsciiMetadataValue;
+
+static PRIVATE_KEY: Lazy<Vec<u8>> = Lazy::new(|| {
+    STANDARD
+        .decode(std::env::var("PRIVATE_ENCRYPTION_KEY").unwrap())
+        .unwrap()
+});
+
+///The recommendation to use a 12-byte nonce for AES-GCM encryption comes from
+/// the National Institute of Standards and Technology (NIST)
+/// which specifies the algorithm in Special Publication 800-38D:
+/// "Recommendation for Block Cipher Modes of Operation: Galois/Counter Mode (GCM) and GMAC"
+/// (source: https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf).
+const NONCE_SIZE: usize = 12;
+
+/// The nonce length when it is encoded to base 64
+const NONCE_LENGTH: usize = ((NONCE_SIZE + 2) / 3) * 4;
 
 #[derive(Clone)]
 pub struct Credentials {
@@ -17,16 +44,7 @@ impl Serialize for Credentials {
         S: serde::Serializer,
     {
         let mut state = serializer.serialize_struct("Credentials", 1)?;
-        state.serialize_field(
-            "metadata",
-            &self
-                .get_metadata()
-                .to_str()
-                .unwrap_or_default()
-                .split_ascii_whitespace()
-                .nth(1)
-                .unwrap(),
-        )?;
+        state.serialize_field("metadata", &self.get_metadata())?;
 
         state.end()
     }
@@ -103,7 +121,7 @@ impl Credentials {
         }
     }
 
-    pub fn get_metadata(&self) -> AsciiMetadataValue {
+    pub fn get_auth(&self) -> AsciiMetadataValue {
         format!(
             "Basic {}",
             URL_SAFE.encode(format!("{}:{}", self.username, self.password))
@@ -112,12 +130,31 @@ impl Credentials {
         .unwrap()
     }
 
+    /// Metadata format (hopefully)
+    /// 0-15: Nonce, Base64 encoded
+    /// rest: encrypted creds, Base64 decode before decrypting
+    pub fn get_metadata(&self) -> String {
+        let cipher = Aes256Gcm::new(PRIVATE_KEY.as_slice().into());
+        let (nonce, metadata) =
+            Self::encrypt(&cipher, &format!("{}:{}", self.username(), self.password()));
+
+        let s = format!("{}{}", STANDARD.encode(nonce), STANDARD.encode(metadata));
+        s
+    }
+
     pub fn from_metadata(metadata: String) -> Result<Self, DecodeError> {
-        let decoded = URL_SAFE.decode(metadata)?;
+        let cipher = Aes256Gcm::new(PRIVATE_KEY.as_slice().into());
+
+        // 12 byte nonce corresponds to 16 length string
+        // First 16 bytes encoded into base64 is the nonce
+        let nonce = STANDARD.decode(&metadata[0..NONCE_LENGTH]).unwrap();
+        let secret = STANDARD.decode(&metadata[NONCE_LENGTH..]).unwrap();
+        let secret = Self::decrypt(&cipher, nonce, secret);
+        println!("{:?}", secret);
 
         // 58 is the ASCII code for ':'
         // metadata is in the form username:password
-        let mut split_at_colon = decoded.split(|ascii| ascii == &58u8);
+        let mut split_at_colon = secret.split(|ascii| ascii == &58u8);
 
         Ok(Self::new(
             String::from_utf8_lossy(split_at_colon.next().unwrap()),
@@ -131,6 +168,31 @@ impl Credentials {
 
     pub fn password(&self) -> &str {
         self.password.as_ref()
+    }
+
+    // Thanks to @ditsuke for the crypto stuff
+    /// Returns (nonce, encrypted data)
+    fn encrypt<S: AsRef<str>>(cipher: &Aes256Gcm, secret: S) -> (Vec<u8>, Vec<u8>) {
+        let mut rng = OsRng;
+
+        // 12 byte nonce
+        let mut nonce = [0u8; NONCE_SIZE];
+        rng.fill_bytes(&mut nonce);
+
+        let encrypted_credentials = cipher
+            .encrypt(Nonce::from_slice(&nonce), secret.as_ref().as_ref())
+            .unwrap();
+
+        (nonce.into(), encrypted_credentials)
+    }
+
+    fn decrypt<S: AsRef<[u8]>>(cipher: &Aes256Gcm, nonce: S, encrypted: S) -> Vec<u8> {
+        cipher
+            .decrypt(
+                Nonce::from_slice(nonce.as_ref()),
+                encrypted.as_ref().as_ref(),
+            )
+            .unwrap()
     }
 }
 
