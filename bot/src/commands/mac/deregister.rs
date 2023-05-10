7f7b5db0b5c30ff825d36638059de6bd993347de
Blonteractor <priyanv2003@gmail.com>
2023-05-10 11:09:29 +0530

    feat: Add MAC DeRegister command

--- bot/src/commands/mac/deregister.rs
+++ bot/src/commands/mac/deregister.rs
@@ -2,25 +2,25 @@ use super::is_valid_mac;
 use crate::{CommandResult, Context, Result};
 use amizone::api::client::UserClient;
 
-static WIFIMACREGISTER_HELP: &str = "/registerwifimac - Register a WiFi MAC address.\n\n\
-        Usage: /registerwifimac [mac_address]\n\n\
-        Alias: /wr [mac_address]\n\n\
+static WIFIMACDEREGISTER_HELP: &str = "/deregisterwifimac - DeRegister a WiFi MAC address.\n\n\
+        Usage: /deregisterwifimac [mac_address]\n\n\
+        Alias: /wd [mac_address]\n\n\
         Arguments:\n\
-        - [mac_address]: The MAC address to register.\n\n\
+        - [mac_address]: The MAC address to deregister.\n\n\
         Example:\n\
         /registerwifimac 00:11:22:33:44:55\n\
         /wr 00:11:22:33:44:55\n\n\
-        Note: The MAC address must be in the following format: `XX:XX:XX:XX:XX:XX` or `XX-XX-XX-XX-XX-XX`. This command also ignores \
-        the 2 adress limit completely and allows you to register unlimited adresses.";
+        Note: The MAC address must be in the following format: `XX:XX:XX:XX:XX:XX` or `XX-XX-XX-XX-XX-XX`. It must also \
+        be already registered on amizone, use the `/wifimacinfo` command to see what adresses are registered.";
 
-/// Register a WiFi MAC address to be able to use Amity WiFi.
+///  DeRegister a WiFi MAC address.
 #[poise::command(
     prefix_command,
     slash_command,
-    help_text_fn = "wifimacregister_help",
-    aliases("wr")
+    help_text_fn = "wifimacderegister_help",
+    aliases("wd")
 )]
-pub async fn registerwifimac(ctx: Context<'_>, address: String) -> CommandResult {
+pub async fn deregisterwifimac(ctx: Context<'_>, address: String) -> CommandResult {
     ctx.defer().await?;
 
     if is_valid_mac(&address) {
@@ -32,8 +32,8 @@ pub async fn registerwifimac(ctx: Context<'_>, address: String) -> CommandResult
     let mut invocation_data = ctx.invocation_data::<Result<UserClient>>().await.unwrap();
 
     let client = invocation_data.as_mut()?;
-    client.register_wifi_mac(address).await?;
-    ctx.say("Registered the MAC succesfully.").await?;
+    client.deregister_wifi_mac(address).await?;
+    ctx.say("DeRegistered the MAC succesfully.").await?;
 
     let wifimac = client.get_wifi_mac_info().await?;
     let addresses = wifimac.addresses.join("`, `");
@@ -49,6 +49,6 @@ pub async fn registerwifimac(ctx: Context<'_>, address: String) -> CommandResult
     Ok(())
 }
 
-fn wifimacregister_help() -> String {
-    WIFIMACREGISTER_HELP.into()
+fn wifimacderegister_help() -> String {
+    WIFIMACDEREGISTER_HELP.into()
 }
--- bot/src/commands/mac/mod.rs
+++ bot/src/commands/mac/mod.rs
@@ -1,2 +1,11 @@
+use regex::Regex;
+
+pub mod deregister;
 pub mod info;
 pub mod register;
+
+// https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
+pub fn is_valid_mac(addr: impl AsRef<str>) -> bool {
+    let mac_regex = Regex::new(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$").unwrap();
+    mac_regex.is_match(addr.as_ref())
+}
